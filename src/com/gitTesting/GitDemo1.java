26d9d7f74b045b3b6c64d2135548294acb27e587
neeraj <neerajchaubey057@gmail.com>
2022-01-24 15:21:31 +0530

    this is updated code commit

--- src/com/gitTesting/GitDemo1.java
+++ src/com/gitTesting/GitDemo1.java
@@ -2,6 +2,9 @@ package com.gitTesting;
 
 public class GitDemo1 {
 
+		public static void hello() {
+			System.out.println("I am static method");
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello java");
